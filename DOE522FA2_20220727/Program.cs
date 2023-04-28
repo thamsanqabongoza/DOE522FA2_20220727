9e9c1fe854366876a9456badd586ff50b16ff41d
thamsanqabongoza <131814334+thamsanqabongoza@users.noreply.github.com>
2023-04-28 23:01:06 +0200

    Update Program.cs
    Trying to do a pull request...

--- DOE522FA2_20220727/Program.cs
+++ DOE522FA2_20220727/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to, DOE522FA2!");
+Console.WriteLine("Where there is a world there is a way!");
+Console.WriteLine("In him we Trust and have our being!");
